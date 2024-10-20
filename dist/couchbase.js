486c38acbbc9d71b72ce89641100141bb6dee00b
Ward Nijman <wardnijman@gmail.com>
2024-10-20 22:24:36 +0200

    Couchbase exports explicit with export star in couchbase.js

--- dist/couchbase.js
+++ dist/couchbase.js
@@ -34,32 +34,45 @@ import * as vectorsearch from "./vectorsearch.js";
 import * as viewexecutor from "./viewexecutor.js";
 import * as viewindexmanager from "./viewindexmanager.js";
 import * as viewtypes from "./viewtypes.js";
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
-    if (k2 === undefined)
-        k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-        desc = { enumerable: true, get: function () { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function (o, m, k, k2) {
-    if (k2 === undefined)
-        k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function (m, exports) {
-    for (var p in m)
-        if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p))
-            __createBinding(exports, m, p);
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const binding_1 = __importDefault(binding);
+
+// Exporting everything explicitly from each module
+export * from "./analyticsindexmanager.js";
+export * from "./analyticstypes.js";
+export * from "./authenticators.js";
+export * from "./binarycollection.js";
+export * from "./bucket.js";
+export * from "./bucketmanager.js";
+export * from "./collection.js";
+export * from "./collectionmanager.js";
+export * from "./crudoptypes.js";
+export * from "./datastructures.js";
+export * from "./diagnosticstypes.js";
+export * from "./errorcontexts.js";
+export * from "./errors.js";
+export * from "./eventingfunctionmanager.js";
+export * from "./generaltypes.js";
+export * from "./mutationstate.js";
+export * from "./queryindexmanager.js";
+export * from "./querytypes.js";
+export * from "./scope.js";
+export * from "./scopesearchindexmanager.js";
+export * from "./sdspecs.js";
+export * from "./searchfacet.js";
+export * from "./searchindexmanager.js";
+export * from "./searchquery.js";
+export * from "./searchsort.js";
+export * from "./searchtypes.js";
+export * from "./streamablepromises.js";
+export * from "./transactions.js";
+export * from "./transcoders.js";
+export * from "./usermanager.js";
+export * from "./vectorsearch.js";
+export * from "./viewexecutor.js";
+export * from "./viewindexmanager.js";
+export * from "./viewtypes.js";
+
 /**
- * Acts as the entrypoint into the rest of the library.  Connecting to the cluster
+ * Acts as the entry point into the rest of the library. Connecting to the cluster
  * and exposing the various services and features.
  *
  * @param connStr The connection string to use to connect to the cluster.
@@ -67,69 +80,33 @@ const binding_1 = __importDefault(binding);
  * @param callback A node-style callback to be invoked after execution.
  * @category Core
  */
-async function connect(connStr, options, callback) {
+export async function connect(connStr, options, callback) {
     return cluster_1.Cluster.connect(connStr, options, callback);
 }
+
 /**
  * Volatile: This API is subject to change at any time.
  *
- * Exposes the underlying couchbase++ library protocol logger.  This method is for
+ * Exposes the underlying couchbase++ library protocol logger. This method is for
  * logging/debugging purposes and must be used with caution as network details will
  * be logged to the provided file.
  *
  * @param filename Name of file protocol logger will save logging details.
  */
-function enableProtocolLoggerToSaveNetworkTrafficToFile(filename) {
-    binding_1.default.enableProtocolLogger(filename);
+export function enableProtocolLoggerToSaveNetworkTrafficToFile(filename) {
+    binding.enableProtocolLogger(filename);
 }
+
 /**
  * Volatile: This API is subject to change at any time.
  *
  * Shutdowns the underlying couchbase++ logger.
- *
  */
-function shutdownLogger() {
-    binding_1.default.shutdownLogger();
+export function shutdownLogger() {
+    binding.shutdownLogger();
 }
-__exportStar(analyticsindexmanager, exports);
-__exportStar(analyticstypes, exports);
-__exportStar(authenticators, exports);
-__exportStar(binarycollection, exports);
-__exportStar(bucket, exports);
-__exportStar(bucketmanager, exports);
-__exportStar(cluster_1, exports);
-__exportStar(collection, exports);
-__exportStar(collectionmanager, exports);
-__exportStar(crudoptypes, exports);
-__exportStar(datastructures, exports);
-__exportStar(diagnosticstypes, exports);
-__exportStar(errorcontexts, exports);
-__exportStar(errors, exports);
-__exportStar(eventingfunctionmanager, exports);
-__exportStar(generaltypes, exports);
-__exportStar(mutationstate, exports);
-__exportStar(queryindexmanager, exports);
-__exportStar(querytypes, exports);
-__exportStar(scope, exports);
-__exportStar(scopesearchindexmanager, exports);
-__exportStar(sdspecs, exports);
-__exportStar(searchfacet, exports);
-__exportStar(searchindexmanager, exports);
-__exportStar(searchquery, exports);
-__exportStar(searchsort, exports);
-__exportStar(searchtypes, exports);
-__exportStar(streamablepromises, exports);
-__exportStar(transactions, exports);
-__exportStar(transcoders, exports);
-__exportStar(usermanager, exports);
-__exportStar(vectorsearch, exports);
-__exportStar(viewexecutor, exports);
-__exportStar(viewindexmanager, exports);
-__exportStar(viewtypes, exports);
-const connect$0 = void 0;
-export const lcbVersion = binding_1.default.cbppVersion;
-export const cbppVersion = binding_1.default.cbppVersion;
-export const cbppMetadata = binding_1.default.cbppMetadata;
-export { connect$0 as connect };
-export { enableProtocolLoggerToSaveNetworkTrafficToFile };
-export { shutdownLogger };
+
+// Constants
+export const lcbVersion = binding.cbppVersion;
+export const cbppVersion = binding.cbppVersion;
+export const cbppMetadata = binding.cbppMetadata;
